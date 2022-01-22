4939e896532c86429b58b4c0d838752f95698900
xtream2000c <carlmarcast@gmail.com>
2022-01-22 20:31:38 +0100

    EJ2 completado totalmente

--- EJ2/JS/script.js
+++ EJ2/JS/script.js
@@ -15,12 +15,21 @@ $(document).ready(function(){
         if(imagenes%7==0){
             filas++;
 
-            $("table").append("<tr id=fila"+ filas +"><td id= "+ imagenes +"><img src='https://loremflickr.com/200/200'></td></tr>");
+            $("table").append("<tr id=fila"+ filas +"><td id= "+ imagenes +" style='display:none'><img src='https://loremflickr.com/200/200?timestamp=" + new Date().getTime()+"'></td></tr>");
+          
+            
+            $("#"+imagenes).fadeIn(1000);
+
+            imagenes++;
+
+
+        }else{
+            $("#fila"+filas).append("<td id= '"+ imagenes +"' style='display:none'><img src='https://loremflickr.com/200/200?timestamp=" + new Date().getTime()+"'></td>");
+        
+            $("#"+imagenes).fadeIn(1000);
             
             imagenes++;
-        }else{
-            $("#fila"+filas).append("<td id= "+ imagenes +"><img src='https://loremflickr.com/200/200'></td>");
-            imagenes++;
+
         }
     })
 
@@ -29,11 +38,15 @@ $(document).ready(function(){
         if (imagenes>=0) {
             
             imagenes--;
-            $("#"+imagenes).remove();
+            $("#"+imagenes).fadeOut(1000, function () {
+                $(this).remove();
+            });
 
             if(imagenes%7==0){
 
-                $("#fila"+ filas).remove();
+                $("#fila"+ filas).fadeOut(1000, function () {
+                    $(this).remove();
+                });
                 filas--;
 
             }
